55a30018a6903f3953df641d81cf0cd1277f4d88
Tavrin <tavrinn@hotmail.com>
2019-10-11 16:54:37 +1100

    added carpark view holder

--- app/src/main/java/com/monash/monashuniapp/Views/MainScreenAdapter.kt
+++ app/src/main/java/com/monash/monashuniapp/Views/MainScreenAdapter.kt
@@ -1,10 +1,16 @@
 package com.monash.monashuniapp.Views
 
+import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
+import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.monash.monashuniapp.Models.BusList
 import com.monash.monashuniapp.Models.CarParkList
 import com.monash.monashuniapp.Models.LectureList
+import com.monash.monashuniapp.R
 
 class MainScreenAdapter(// Internal data
     private val lectureList: LectureList,
@@ -12,6 +18,49 @@ class MainScreenAdapter(// Internal data
     private val busList: BusList
 ) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
+
+    class CarParkViewHolder(itemView: View, private val cpList: CarParkList) :
+        RecyclerView.ViewHolder(itemView) {
+        // View for the carpark color codes.
+        private val cpColorCodes: LinearLayout
+        private val cpFreeSpaces: TextView
+
+        // Internal color code array for the circles
+        private val colorList =
+            arrayOf("#0000FF", "#9933ff", "#cc00ff", "#ff0000", "#ffff66", "#00cc00", "#ff9900")
+
+        init {
+
+            // CarPark Stuff here
+            cpColorCodes = itemView.findViewById(R.id.car_parks_color_codes)
+            cpFreeSpaces = itemView.findViewById(R.id.car_parks_free_spaces)
+
+            drawCarParkColorCodes()
+            setFreeSpaces(cpList.response[0].freeSpace)
+        }
+
+        private fun drawCarParkColorCodes() {
+            cpColorCodes.removeAllViews()
+
+            for (i in 0..6) {
+                val view = View(cpColorCodes.context)
+                val lp = LinearLayout.LayoutParams(32, 32)
+                lp.setMargins(0, 0, 8, 0)
+                view.layoutParams = lp
+
+                val shape = GradientDrawable()
+                shape.shape = GradientDrawable.OVAL
+                shape.setColor(Color.parseColor(colorList[i]))
+                view.background = shape
+
+                cpColorCodes.addView(view)
+            }
+        }
+
+        private fun setFreeSpaces(freeSpaces: String) {
+            cpFreeSpaces.text = freeSpaces
+        }
+    }
 
 
     // Create new views (invoked by the layout manager)
@@ -21,7 +70,7 @@ class MainScreenAdapter(// Internal data
     }
 
 
-    
+
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
 
     }
