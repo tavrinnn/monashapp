75a6cc2d4ab214bb2ccee2de69291d4b30c81574
Tavrin <tavrinn@hotmail.com>
2019-10-11 15:07:42 -0700

    documenting how an API flow would take place if it was actually set up

--- app/src/main/java/com/monash/monashuniapp/MainActivity.kt
+++ app/src/main/java/com/monash/monashuniapp/MainActivity.kt
@@ -5,8 +5,22 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    // If we were connecting to an API we would define our retrofit builder up here which pointed to
+    // an interface which defines all our endpoints we want to access. The compositedisopsable would be defined here too.
+
+
+
+    /* void getdata()
+     * inside this function the requestinteface would be added, we would add the observable to the main thread, subscribeon IO scheduler and the subscribe callback would point to
+     * our displayData() function.
+     */
+
+
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // At this point, we would display some loading screen and set up our observable to subscribe to the api transaction. Lets call this function getData()
     }
 }
